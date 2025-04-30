94f3f4595b44a54d73dc1e82e2558d42385f2631
Shmantsar Angelina <Angelina.Shmantsar@urfu.me>
2025-04-30 19:02:58 +0500

    Добавлен компонент ячейки и анимации для него

---
+++ tic-tac-toe/src/components/Cell/Cell.tsx
@@ -0,0 +1,21 @@
+import Lottie from 'lottie-react';
+import { FC, memo } from 'react';
+import crossAnimation from '../../assets/cross.json';
+import ovalAnimation from '../../assets/oval.json';
+
+import styles from './Cell.module.css';
+
+type CellProps = {
+    readonly value: 'X' | 'O' | null;
+    readonly onClick: () => void;
+}
+
+const CellComponent: FC<CellProps> = ({ value, onClick }) => {
+    return (
+        <button onClick={onClick} type='button' className={styles.cell}>
+            <Lottie animationData={value === 'X' ? crossAnimation : ovalAnimation} loop={false} />
+        </button>
+    )
+};
+
+export const Cell = memo(CellComponent);
